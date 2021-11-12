15ce837637da3e18b002fed695d302c9d66246c4
higakijin <hm385.chejptks@gmail.com>
2021-11-13 01:50:57 +0900

    認証機能の追加（バックエンド）

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
   root to: 'pages#index'
-  get '*path', to: 'pages#index'
-  
+
+  mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+    registrations: 'auth/registrations'
+  }
+
   # Routerのmodeがhashの場合は不要です
   # get '/article/:id', to: 'pages#index'
+  get '*path', to: 'pages#index'
 end
