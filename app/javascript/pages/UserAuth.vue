4b8f61e904987e50b3f141b559cdaf4f041ac61c
higakijin <hm385.chejptks@gmail.com>
2021-11-13 15:38:27 +0900

    サインアップ処理の実装

--- app/javascript/pages/UserAuth.vue
+++ app/javascript/pages/UserAuth.vue
@@ -10,7 +10,7 @@
               <br>未登録なら<span @click="showLogin = false" class="text-blue-700 underline cursor-pointer">こちら</span>をクリック
             </p>
           </div>
-          <form @submit.prevent="signUp" class="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
+          <form @submit.prevent="login" class="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
             <h2 class="text-gray-900 text-lg font-medium title-font mb-5">ログイン</h2>
             <div class="relative mb-4">
               <label for="email" class="leading-7 text-sm text-gray-600">メールアドレス</label>
@@ -39,23 +39,23 @@
               <br>登録済なら<span @click="showLogin = true" class="text-blue-700 underline cursor-pointer">こちら</span>をクリック
             </p>
           </div>
-          <form class="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
+          <form @submit.prevent="signUp" class="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
             <h2 class="text-gray-900 text-lg font-medium title-font mb-5">サインアップ</h2>
             <div class="relative mb-4">
               <label for="name" class="leading-7 text-sm text-gray-600">名前</label>
-              <input type="text" id="name" name="name" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+              <input type="text" v-model="signupName" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
             </div>
             <div class="relative mb-4">
               <label for="email" class="leading-7 text-sm text-gray-600">メールアドレス</label>
-              <input type="email" id="email" name="email" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+              <input type="email" v-model="signupEmail" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
             </div>
             <div class="relative mb-4">
               <label for="password" class="leading-7 text-sm text-gray-600">パスワード</label>
-              <input type="password" id="password" name="password" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+              <input type="password" v-model="signupPassword" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
             </div>
             <div class="relative mb-4">
               <label for="password_confirmation" class="leading-7 text-sm text-gray-600">パスワード（確認用）</label>
-              <input type="password" id="password_confirmation" name="password_confirmation" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+              <input type="password" v-model="signupPasswordConfirmation" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
             </div>
             <button class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">登録</button>
           </form>
@@ -76,13 +76,19 @@ export default {
   data () {
     return {
       showLogin: true,
+
       loginEmail: '',
       loginPassword: '',
+
+      signupName: '',
+      signupEmail: '',
+      signupPassword: '',
+      signupPasswordConfirmation: '',
       error: null
     }
   },
   methods: {
-    async signUp() {
+    async login() {
       this.error = null
       try {
         const res = await axios.post('http://localhost:3000/auth/sign_in', {
@@ -96,6 +102,24 @@ export default {
         return res
       } catch (error) {
         this.error = 'ログインできませんでした'
+      }
+    },
+    async signUp() {
+      this.error = null
+      try {
+        const res = await axios.post('http://localhost:3000/auth', {
+          name: this.signupName,
+          email: this.signupEmail,
+          password: this.signupPassword,
+          password_confirmation: this.signupPasswordConfirmation,
+        })
+        if (!res) {
+          throw new Error('サインアップできませんでした。')
+        }
+        console.log({res});
+        return res
+      } catch (error) {
+        this.error = 'サインアップできませんでした。'
       }
     }
   }
