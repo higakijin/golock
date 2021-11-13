0e7125266e476ec68a7fea2a3e0dec5cbf69eaf6
higakijin <hm385.chejptks@gmail.com>
2021-11-13 13:52:53 +0900

    ログイン処理の実装

--- app/javascript/pages/UserAuth.vue
+++ app/javascript/pages/UserAuth.vue
@@ -1,27 +1,102 @@
 <template>
   <div>
-    <p>ようこそ。こちらはログインページ</p>
-    <div v-if="showLogin">
-      <LoginForm />
-      <p>未登録なら<span @click="showLogin = false">こちら</span>をクリック</p>
+    <div v-if="showLogin" class="loginForm">
+      <section class="text-gray-600 body-font">
+        <div class="container px-5 py-64 mx-auto flex flex-wrap items-center">
+          <div class="lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0">
+            <h1 class="title-font font-medium text-3xl text-gray-900">Golockにログインしてあなたの知識を世界中に伝えよう。</h1>
+            <p class="leading-relaxed mt-4">
+              あなたのアウトプットを待っている人がいます。いますぐ始めよう。
+              <br>未登録なら<span @click="showLogin = false" class="text-blue-700 underline cursor-pointer">こちら</span>をクリック
+            </p>
+          </div>
+          <form @submit.prevent="signUp" class="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
+            <h2 class="text-gray-900 text-lg font-medium title-font mb-5">ログイン</h2>
+            <div class="relative mb-4">
+              <label for="email" class="leading-7 text-sm text-gray-600">メールアドレス</label>
+              <input type="text" required v-model="loginEmail" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+            </div>
+            <div class="relative mb-4">
+              <label for="password" class="leading-7 text-sm text-gray-600">パスワード</label>
+              <input type="password" required v-model="loginPassword" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+            </div>
+            <div class="relative mb-4 text-red-500">
+              {{ error }}
+            </div>
+            <button class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">登録</button>
+          </form>
+        </div>
+      </section>
+      
     </div>
-    <div v-if="!showLogin">
-      <SignupForm />
-      <p>登録済なら<span @click="showLogin = true">こちら</span>をクリック</p>
+    <div v-if="!showLogin" class="signupForm">
+      <section class="text-gray-600 body-font">
+        <div class="container px-5 py-44 mx-auto flex flex-wrap items-center">
+          <div class="lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0">
+            <h1 class="title-font font-medium text-3xl text-gray-900">ようこそ、Golockへ</h1>
+            <p class="leading-relaxed mt-4">
+              登録してあなたのアウトプットを公開しましょう。あなたの知識を求めている人がいます。
+              <br>登録済なら<span @click="showLogin = true" class="text-blue-700 underline cursor-pointer">こちら</span>をクリック
+            </p>
+          </div>
+          <form class="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
+            <h2 class="text-gray-900 text-lg font-medium title-font mb-5">サインアップ</h2>
+            <div class="relative mb-4">
+              <label for="name" class="leading-7 text-sm text-gray-600">名前</label>
+              <input type="text" id="name" name="name" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+            </div>
+            <div class="relative mb-4">
+              <label for="email" class="leading-7 text-sm text-gray-600">メールアドレス</label>
+              <input type="email" id="email" name="email" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+            </div>
+            <div class="relative mb-4">
+              <label for="password" class="leading-7 text-sm text-gray-600">パスワード</label>
+              <input type="password" id="password" name="password" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+            </div>
+            <div class="relative mb-4">
+              <label for="password_confirmation" class="leading-7 text-sm text-gray-600">パスワード（確認用）</label>
+              <input type="password" id="password_confirmation" name="password_confirmation" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+            </div>
+            <button class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">登録</button>
+          </form>
+        </div>
+      </section>
     </div>
 
   </div>
 </template>
 
 <script>
-import LoginForm from '../components/UserAuth/LoginForm.vue'
-import SignupForm from '../components/UserAuth/SignupForm.vue'
+// import LoginForm from '../components/UserAuth/LoginForm.vue'
+// import SignupForm from '../components/UserAuth/SignupForm.vue'
+import axios from 'axios'
 
 export default {
-  components: {LoginForm, SignupForm},
+  // components: {LoginForm, SignupForm},
   data () {
     return {
-      showLogin: true
+      showLogin: true,
+      loginEmail: '',
+      loginPassword: '',
+      error: null
+    }
+  },
+  methods: {
+    async signUp() {
+      this.error = null
+      try {
+        const res = await axios.post('http://localhost:3000/auth/sign_in', {
+          email: this.loginEmail,
+          password: this.loginPassword
+        })
+        if (!res) {
+          throw new Error('ログインできませんでした')
+        }
+        console.log({res});
+        return res
+      } catch (error) {
+        this.error = 'ログインできませんでした'
+      }
     }
   }
 }
