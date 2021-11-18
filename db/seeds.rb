2ca2b77c3184119134737728e5c789ff17a08c76
higakijin <hm385.chejptks@gmail.com>
2021-11-18 23:41:36 +0900

    seedのパブリックをtrueへ

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
 3.times do |n|
-  Post.create(title: "#{n}回目の投稿タイトル", body: "#{n}回目の投稿ボディです！", user_id: User.first.id)
+  Post.create(title: "#{n}回目の投稿タイトル", body: "#{n}回目の投稿ボディです！", user_id: User.first.id, published: true)
 end
