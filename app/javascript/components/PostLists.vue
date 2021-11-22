c9ee16bf53f64288336c76579035a3c32bcd938c
higakijin <hm385.chejptks@gmail.com>
2021-11-22 09:53:58 +0900

    ログインしているユーザーの名前を表示

--- app/javascript/components/PostLists.vue
+++ app/javascript/components/PostLists.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="text-gray-600 body-font overflow-hidden">
-    <div class="container px-5 py-24 mx-auto">
+    <div class="container px-5 py-12 mx-auto">
       <div class="-my-8 divide-y-2 divide-gray-100">
         <div v-for='post in posts' :key="post.id">
           <div class="py-8 flex flex-wrap md:flex-nowrap">
