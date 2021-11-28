65d28a63350bb5efb6e37df315ef41673ed0065b
higakijin <hm385.chejptks@gmail.com>
2021-11-29 01:40:21 +0900

    purgeの設定（修正）

--- app/javascript/css/tailwind.js
+++ app/javascript/css/tailwind.js
@@ -1,8 +1,7 @@
 module.exports = {
   purge: {
     enable: true,
-    content: ['/home/higakijin/golock/app/javascript/pages/*.vue', '/home/higakijin/golock/app/javascript/components/*.vue']
-
+    content: ['app/javascript/pages/*.vue', 'app/javascript/components/*.vue', ]
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
